cd856884a0dc9c56a2cfe2d4c1b7070311133620
Asa Gayle <asgayle@microsoft.com>
2021-12-06 13:40:24 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as tc from '@actions/tool-cache';
+import {exec} from 'child_process';
+
+// const { exec } = require('child_process');
+// const tc = require('@actions/tool-cache');
+// const fs = require('fs');
+
+async function getHelmTry(): Promise<string> {
+    const getHelmScriptPath =  await tc.downloadTool('https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3');
+    fs.chmodSync(getHelmScriptPath, '700');
+    var runGetHelmScript = exec('bash ./get_helm.sh help', (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if(error !== null){
+        console.log(`exec error: ${error}`)
+    }
+    });
+
+    return "COMPLETE";
+}
+
+
+console.log(getHelmTry());
