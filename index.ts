f4d828e3a83606418d054a59fac06887336e2388
Asa Gayle <asgayle@microsoft.com>
2022-01-03 15:14:00 -0500

    Checking if chmod is needed

--- index.ts
+++ index.ts
@@ -24,7 +24,6 @@ export async function getHelmTry(): Promise<string> {
         throw new Error(util.format("Failed to download get_helm.sh from locations: %s", getHelmDownloadUrl))
     }
     
-    fs.chmodSync(getHelmScriptPath, '666');
     console.log("Current getHelmScriptPath === " + getHelmScriptPath);
     console.log(fs.existsSync(getHelmScriptPath));
     try{
