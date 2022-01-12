c5638a02f5434e7dac96b5f3275b559a610612e1
Asa Gayle <asgayle@microsoft.com>
2022-01-12 10:08:08 -0500

    Initial commit

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import * as os from 'os';
 import * as util from 'util';
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as core from '@actions/core'
+import {exec} from 'child_process';
 
 const HELM_SCRIPT_URL = 'https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3';
 const INPUT_VERSION:string = core.getInput('version', { 'required': true });
 
 export async function run() {
     try{
-        setupHelmViaShell().then(runHelmScript);
+        setupHelmViaShell();
         console.log("COMPLETE");
     } catch(e){
         throw new Error(util.format("Failed to run bash scripts from %s", "getHelmTry()"));
@@ -17,30 +17,62 @@ export async function run() {
 
 export async function setupHelmViaShell(): Promise<void> {
     try{
-        await exec.exec('curl', ['-o', 'get_helm.sh', HELM_SCRIPT_URL]);
+        exec(`curl -o get_helm.sh ${HELM_SCRIPT_URL}`, (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            runHelmScript();
+        });
     } catch (e){
         console.log(`exec error: ${e}`);
     }
     return;
 }
 
-export function runHelmScript(){
-    let script_path = "./get_helm.sh";
+export async function runHelmScript(): Promise<void> {
+    let superU = "";
 
     try{
-        exec.exec('chmod', ['700', 'get_helm.sh']);
+        if(!os.type().match(/^Win/)){
+            superU = "sudo ./";
+            exec("chmod 700 get_helm.sh", (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+            });
+        } else {
+            console.log("WINDOWS \n");
+            exec("choco version", (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+            });
+            exec("chmod 700 get_helm.sh", (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+            });
+        }
 
         if(INPUT_VERSION == "latest"){
-            exec.exec(script_path);
+            exec(util.format('%sget_helm.sh', superU), (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+            });
         } else {
-            INPUT_VERSION[0] !== 'v' ?
-                exec.exec(script_path, ['--version', "v" + INPUT_VERSION]) :
-                exec.exec(script_path, ['--version', INPUT_VERSION])
+            if(INPUT_VERSION[0] !== 'v'){
+                exec(util.format('%sget_helm.sh --version v%s', superU, INPUT_VERSION), (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                });
+            } else {
+                exec(util.format('%sget_helm.sh --version %s}',superU, INPUT_VERSION), (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                });
+            }
         }
     } catch(e){
         console.log(`exec error: ${e}`);
-        throw new Error("NOT RUN");
+        throw new Error("NOT RUN")
     }
+    return;
 }
 
 run().catch(core.setFailed);
